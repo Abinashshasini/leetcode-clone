07fa61d7a79b1398208ddcea325a5aae75a5b8c4
newusername <abinashshasini125@gmail.com>
2024-01-07 18:26:16 +0530

    feat : header created

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  compilerOptions: {
+    target: 'es5',
+    lib: ['dom', 'dom.iterable', 'esnext'],
+    allowJs: true,
+    skipLibCheck: true,
+    strict: true,
+    forceConsistentCasingInFileNames: true,
+    noEmit: true,
+    esModuleInterop: true,
+    module: 'esnext',
+    moduleResolution: 'bundler',
+    resolveJsonModule: true,
+    isolatedModules: true,
+    jsx: 'preserve',
+    incremental: true,
+    plugins: [
+      {
+        name: 'next',
+      },
+    ],
+    paths: {
+      '@/*': ['./*'],
+    },
+  },
+  include: ['next-env.d.ts', '**/*.ts', '**/*.tsx', '.next/types/**/*.ts'],
+  exclude: ['node_modules'],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/components/header/index.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import classes from './style.module.scss';
+
+const Header = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.logo}>
+        <Image src="/assets/logo.png" width={100} height={100} alt="logo" />
+        <h1>Leetcode</h1>
+      </div>
+      <div className={classes.menu}>
+        <div className={classes.menuItem}>Questions</div>
+        <div className={classes.menuItem}>Explore</div>
+        <div className={classes.menuItem}>Product</div>
+        <div className={classes.menuItem}>Develpper</div>
+        <div className={classes.menuItem}>Sign in</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
